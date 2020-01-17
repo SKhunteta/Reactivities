fd4e99f3fc5f29416ae72314e00204075ba304ec
SKhunteta <shreyans.khunteta@gmail.com>
2020-01-17 14:10:01 -0800

    fixed bug, refactored everything

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,26 +1,15 @@
 import React, { useContext } from 'react';
 import { Item, Button, Label, Segment } from 'semantic-ui-react';
-import { IActivity } from '../../../app/models/activity';
-import {observer} from 'mobx-react-lite'
-import ActivityStore from '../../../app/stores/activityStore'
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
 
-interface IProps {
-  deleteActivity: (id: string) => void;
-  submitting: boolean
-  target: string;
-}
-
-const ActivityList: React.FC<IProps> = ({
-  deleteActivity,
-  submitting,
-  target
-}) => {
+const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
-  const {activities, selectActivity} = activityStore;
+  const {activitiesByDate, selectActivity, deleteActivity, submitting, target} = activityStore;
   return (
     <Segment clearing>
       <Item.Group divided>
-        {activities.map(activity => (
+        {activitiesByDate.map(activity => (
           <Item key={activity.id}>
             <Item.Content>
               <Item.Header as='a'>{activity.title}</Item.Header>
@@ -39,9 +28,9 @@ const ActivityList: React.FC<IProps> = ({
                   color='blue'
                 />
                 <Button
-                name={activity.id}
-                loading={target === activity.id && submitting}
-                  onClick={() => deleteActivity(activity.id)}
+                  name={activity.id}
+                  loading={target === activity.id && submitting}
+                  onClick={(e) => deleteActivity(e, activity.id)}
                   floated='right'
                   content='Delete'
                   color='red'
