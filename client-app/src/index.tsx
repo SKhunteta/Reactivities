2b4ebba12603b942779af9cd0c0561ea424e7724
SKhunteta <shreyans.khunteta@gmail.com>
2020-01-18 13:06:57 -0800

    set up links

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,6 +4,7 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 ReactDOM.render(
 <BrowserRouter>
