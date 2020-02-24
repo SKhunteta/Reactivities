2ba3e6d139c390e728a5f0d31a4b6a7da67d9d0c
Shrey <>
2020-02-23 17:13:53 -0800

    decided not to use data annotations

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -12,19 +12,18 @@ namespace Application.Activities
     {
         public class Command : IRequest
         {
-            [Required]
             public Guid Id { get; set; }
-            [Required]
+            
             public string Title { get; set; }
-            [Required]
+            
             public string Description { get; set; }
-            [Required]
+           
             public string Category { get; set; }
-            [Required]
+           
             public DateTime Date { get; set; }
-            [Required]
+          
             public string City { get; set; }
-            [Required]
+         
             public string Venue { get; set; }
         }
 
