bfea74c9a96d08f8617b3cf676699eff60ca537c
Shrey <shreyans.khunteta@gmail.com>
2020-02-23 17:38:37 -0800

    added fluent validation to Edit

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -32,7 +32,12 @@ namespace Application.Activities
         public class CommandValidator: AbstractValidator<Command> {
             public CommandValidator() 
             {
-                RuleForEach(x => x.Title).NotEmpty();
+                RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
             }
         }
 
